bd04eebb24a66dc9d94f1b878bc6025e491d716b
ItsEmilianoSM <A24310389@live.ceti.mx>
2025-10-04 00:23:21 +0000

    Practica 2

---
+++ src/memoria.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class Empleado{
+    private: //Nacen de la relacion tiene
+        char nombre[10]; //10B
+        int edad; //4B
+        char direccion[255]; //255B
+        int telefono; //4B
+    public: //Nacen de la relacion usa
+        int LeerEdad(){
+            return edad;
+        }
+}; //273B
+
+int main(int argc, char const *argv[])
+{
+    //Tamaños de memoria estatica
+    //Enteros
+    cout << "Tamaño de int: " << sizeof(int) << endl;
+    cout << "Tamaño de long: " << sizeof(long) << endl;
+    //Punto Flotante
+    cout << "Tamaño de float: " << sizeof(float) << endl;
+    cout << "Tamaño de double: " << sizeof(double) << endl;
+    //Bytes
+    cout << "Tamaño de char: " << sizeof(char) << endl;
+    cout << "Tamaño de byte: " << sizeof(byte) << endl;
+    cout << "Tamaño de bool: " << sizeof(bool) << endl;
+    //Punteros
+    cout << "Tamaño de puntero: " << sizeof(int *) << endl;
+    //Tamaños tipo NO nativos
+    Empleado empleados [10]; //2730B
+    cout << "Tamaño de Empleado: " << sizeof(Empleado) << endl;
+    cout << "Empleados: " << empleados << endl;
+    cout << "Empleados: " << empleados[0].LeerEdad() << endl;
+
+    //Casteos
+    int a = 37;
+    char b = (char)a;
+    cout << b <<endl;
+
+    return 0;
+}
